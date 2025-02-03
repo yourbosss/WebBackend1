101becc4c50cffea1c5313fdb548810976e31cd5
BIG BO$$$ <kseniia.bosss@gmail.com>
2025-02-03 23:05:14 +0700

    connection to the server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-import express, { Response, Request } from "express";
-import { request } from "http";
+import mongoose from "mongoose";
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (request: Request, response: Response) => {
-  response.status(200).send("Hello Express!");
-});
+const start = async () => {
+  try {
+    // Подключение к MongoDB.
+    await mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000");
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log("Connected to MongoDB");
+
+    app.use(express.json());
+
+    app.get("/", (req: Request, res: Response) => {
+      res.send("Hello, Express with TypeScript!");
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  }
+};
+
+start();
